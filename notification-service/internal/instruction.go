a58b286ad77c1889cff304c41808f54f9319899a
arthadede <arthadede@gmail.com>
2025-10-06 21:55:16 +0800

    FEAT: update type instruction

--- notification-service/internal/instruction.go
+++ notification-service/internal/instruction.go
@@ -3,5 +3,4 @@ package internal
 type InstructionNotification struct {
 	UserID        string `json:"user_id"`
 	InstructionID string `json:"instruction_id"`
-	FileID        string `json:"file_id"`
 }
